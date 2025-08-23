eb47b522f01adeb1b8815a1f9f49c182e8da2efc
Jeter Chan <clocklife0226@gmail.com>
2025-08-23 16:07:28 +0800

    feat: add upate order status API

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,6 @@
 const { getModels } = require('../models');
 
+// 回傳所有訂單
 const getAllOrders = async (req, res) => {
     try {
         const { Order } = getModels();
@@ -20,6 +21,41 @@ const getAllOrders = async (req, res) => {
     }
 };
 
+const updateOrderStatus = async (req, res) => {
+    // api/admin/{orderId}/status -> PATCH method
+    const { orderId } = req.params; // orderNumber
+    const { status } = req.body;
+
+    try {
+        // get order by orderNumber
+        const { Order } = getModels();
+        const orderNeedToUpdate = await Order.findOne({ orderNumber: orderId });
+
+        if(!orderNeedToUpdate) {
+            return res.status(404).json({
+                status:'fail',
+                message:'Order not found'
+            });
+        }
+
+        // if order exists, update status
+        orderNeedToUpdate.status = status;
+
+        await orderNeedToUpdate.save();
+        return res.status(200).json({
+            status: 'success',
+            message: 'Order status updated successfully'
+        });
+    } catch (error) {
+        console.error('❌ [Admin] Error updating order status:', error);
+        return res.status(500).json({
+            status: false,
+            message: 'Update order status failed'
+        });
+    }
+}
+
 module.exports = {
-    getAllOrders
+    getAllOrders,
+    updateOrderStatus
 };
--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 // admin 相關路由
+// get all orders
 router.get('/orders', adminController.getAllOrders);
+// update order status
+router.patch('/orders/:orderId/status', adminController.updateOrderStatus);
 
 module.exports = router;
